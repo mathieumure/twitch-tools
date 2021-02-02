725387a486eae07eab08ed0a892a607a86354f72
Mathieu Mure <mathieu.mure@gmail.com>
2021-02-02 20:20:32 +0100

    style: format and lint code

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,24 +3,21 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
-    'prettier'
-  ],
-  parser: '@typescript-eslint/parser',
+  extends: ["airbnb-base", "prettier"],
+  ignorePatterns: ["public/build"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 2019,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: [
-    '@typescript-eslint',
-    'svelte3',
-    'prettier'
-  ],
+  plugins: ["@typescript-eslint", "svelte3", "prettier"],
   overrides: [
     {
-      files: ['*.svelte'],
-      processor: 'svelte3/svelte3',
+      files: ["*.svelte"],
+      processor: "svelte3/svelte3",
     },
   ],
+  rules: {
+    quotes: ["error", "single"],
+  },
 };
