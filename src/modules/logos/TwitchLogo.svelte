eca95c33486e91d9e6bc632a276e763e82e5870b
Mathieu Mure <mathieu.mure@gmail.com>
2021-02-16 21:00:58 +0100

    feat: add tooltip with social

---
+++ src/modules/logos/TwitchLogo.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export let height: number;
+    export let width: number;
+    export let color: string;
+</script>
+
+<svg height={height} width={width} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" {...$$props}>
+	<path fill={color} d="M48,0L16,96v352h128v64h64l64-64h96l128-136.32V0H48z M464,288l-89.6,96H260.928L192,434.144V384H80V32h384V288z"/>
+  <rect x="240" y="128" fill={color} width="32" height="128"/>
+  <rect x="336" y="128" fill={color} width="32" height="128"/>
+</svg>
---
+++ src/modules/tooltip/Tooltip.svelte
@@ -0,0 +1,74 @@
+<script>
+  import TwitterLogo from "../logos/TwitterLogo.svelte";
+  import TwitchLogo from "../logos/TwitchLogo.svelte";
+  import GithubLogo from '../logos/GithubLogo.svelte';
+  import SocialDisplay from '../social/SocialDisplay.svelte'
+  import DiscordLogo from "../logos/DiscordLogo.svelte";
+</script>
+
+<div class="tooltip">
+  <SocialDisplay>
+    <div class="social" slot="top">
+      <TwitchLogo width="23" height="23" color="#1bbfc4" class="left-logo"/>
+      <p>InfrazFull</p>
+    </div>
+    <div class="social" slot="bottom">
+      <p>@MathieuMure</p>
+      <TwitterLogo width="25" height="25" color="#1bbfc4" class="right-logo"/>
+    </div>
+  </SocialDisplay>
+  <h1>InfrazFull</h1>
+  <SocialDisplay>
+    <div class="social" slot="top">
+      <GithubLogo width="25" height="25" color="#1bbfc4" class="left-logo"/>
+      <p>mathieumure</p>
+    </div>
+    <div class="social" slot="bottom">
+      <p>InfrazFull#7801</p>
+      <DiscordLogo width="25" height="25" color="#1bbfc4" class="right-logo"/>
+    </div>
+  </SocialDisplay>
+</div>
+
+<style>
+  .tooltip {
+      height: 50px;
+      width: 27%;
+      background-color: #3b3b3b;
+      color: #e1e1e1;
+      padding-left: 10px;
+      padding-right: 10px;
+
+      border-bottom-left-radius: 10px;
+      border-bottom-right-radius: 10px;
+      border-left: solid 2px #1bbfc4;
+      border-bottom: solid 2px #1bbfc4;
+      border-right: solid 2px #1bbfc4;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+  }
+
+  h1 {
+      font-size: 1.6rem;
+      flex-grow: 1;
+      text-align: center;
+  }
+
+  .social {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-grow: 1;
+      font-size: 1.1rem;
+  }
+
+  .social :global(.left-logo) {
+      margin-right: 8px;
+  }
+
+  .social :global(.right-logo) {
+      margin-left: 8px;
+  }
+</style>
